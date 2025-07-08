d9fda2201c9a17b9c4eec1ee489295abcdd5e364
CANFIELD.BRANDIN.W.1078680373.E <bcanfield@miletwo.us>
2025-07-08 16:56:51 -0400

    CI

--- packages/database/src/client.ts
+++ packages/database/src/client.ts
@@ -1,15 +1,5 @@
 import { PrismaClient } from "../generated/prisma";
-import { config as loadDotenv } from "dotenv";
-import { resolve } from "node:path";
 
-// Load root .env file
-// Load root .env file
-// const result = loadDotenv({ path: resolve(__dirname, "../../../.env") });
-
-// if (result.error) {
-// 	console.error("⚠️  Failed to load .env:", result.error);
-// 	process.exit(1);
-// }
 const globalForPrisma = global as unknown as { prisma: PrismaClient };
 
 export const prisma = globalForPrisma.prisma || new PrismaClient();
