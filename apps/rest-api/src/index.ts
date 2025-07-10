3ba7666779065af5aaa1b824c1f87e32e574e477
CANFIELD.BRANDIN.W.1078680373.E <bcanfield@miletwo.us>
2025-07-10 12:49:58 -0400

    init with zod validation

--- apps/rest-api/src/index.ts
+++ apps/rest-api/src/index.ts
@@ -1,13 +1,33 @@
 import { Hono } from "hono";
 import { handle as vercelHandle } from "hono/vercel";
+import { zValidator } from "@hono/zod-validator";
+import { z } from "zod";
+import { hc, type InferResponseType, type InferRequestType } from "hono/client";
 
 const app = new Hono().basePath("/api/hono");
 
-app.get("/hello", (c) => {
-	return c.json({
-		ok: true,
-		message: "Hello Hono!",
-	});
+const schema = z.object({
+	id: z.string(),
+	title: z.string(),
 });
 
-export { vercelHandle, app };
+type Todo = z.infer<typeof schema>;
+
+const todos: Todo[] = [];
+
+const route = app
+	.post("/todo", zValidator("form", schema), (c) => {
+		const todo = c.req.valid("form");
+		todos.push(todo);
+		return c.json({
+			message: "created!",
+		});
+	})
+	.get((c) => {
+		return c.json({
+			todos,
+		});
+	});
+type RestApiType = typeof route;
+
+export { vercelHandle, app, type RestApiType, hc, type InferResponseType, type InferRequestType };
--- apps/web/app/test/page.tsx
+++ apps/web/app/test/page.tsx
@@ -1,21 +1,65 @@
 "use client";
+import { useQuery, useMutation, QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { hc, type InferResponseType, type InferRequestType, type RestApiType } from "@cronicorn/rest-api";
 
-import { useEffect, useState } from "react";
+const queryClient = new QueryClient();
+const {
+	api: { hono: restApi },
+} = hc<RestApiType>("");
 
-export default function Home() {
-	const [data, setData] = useState(null);
-	const [isLoading, setLoading] = useState(true);
-
-	useEffect(() => {
-		fetch("/api/hono/hello")
-			.then((res) => res.json())
-			.then((data) => {
-				setData(data);
-				setLoading(false);
-			});
-	}, []);
-
-	if (isLoading) return <p>Loading...</p>;
-	if (!data) return <p>No profile data</p>;
-	return <div>test page: {JSON.stringify(data)}</div>;
+export default function TestPage() {
+	return (
+		<QueryClientProvider client={queryClient}>
+			<Todos />
+		</QueryClientProvider>
+	);
 }
+
+const Todos = () => {
+	const query = useQuery({
+		queryKey: ["todos"],
+		queryFn: async () => {
+			const res = await restApi.todo.$get();
+			return await res.json();
+		},
+	});
+
+	const $post = restApi.todo.$post;
+
+	const mutation = useMutation<InferResponseType<typeof $post>, Error, InferRequestType<typeof $post>["form"]>({
+		mutationFn: async (todo) => {
+			const res = await $post({
+				form: todo,
+			});
+			return await res.json();
+		},
+		onSuccess: async () => {
+			queryClient.invalidateQueries({ queryKey: ["todos"] });
+		},
+		onError: (error) => {
+			console.log(error);
+		},
+	});
+
+	return (
+		<div>
+			<button
+				type="button"
+				onClick={() => {
+					mutation.mutate({
+						id: Date.now().toString(),
+						title: "Write code",
+					});
+				}}
+			>
+				Add Todo
+			</button>
+
+			<ul>
+				{query.data?.todos.map((todo) => (
+					<li key={todo.id}>{todo.title}</li>
+				))}
+			</ul>
+		</div>
+	);
+};
