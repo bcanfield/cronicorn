0bd2260b5160672388d3dd4acd236ae593184cec
CANFIELD.BRANDIN.W.1078680373.E <bcanfield@miletwo.us>
2025-07-17 13:17:23 -0400

    env

--- apps/api/src/app.ts
+++ apps/api/src/app.ts
@@ -59,7 +59,7 @@ const route = app.get(
       },
       servers: [
         {
-          url: process.env.API_EXTERNAL_URL ?? "http://localhost:3001",
+          url: process.env.NEXT_PUBLIC_API_URL ?? "http://localhost:3001",
           description: "Cronicorn API Server",
         },
       ],
