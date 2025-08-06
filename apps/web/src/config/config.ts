f0a4ce23e45c4570862585b5e2dca6e67a854aec
CANFIELD.BRANDIN.W.1078680373.E <bcanfield@miletwo.us>
2025-08-05 23:15:23 -0400

    ENV

--- apps/web/src/config/config.ts
+++ apps/web/src/config/config.ts
@@ -1,3 +1,4 @@
-export const API_URL = "http://localhost:9999/api";
+// eslint-disable-next-line node/no-process-env
+export const API_URL = process.env.NODE_ENV === "development" ? "http://localhost:9999/api" : "https://api.cronicorn.app/api";
 export const DOCS_URL = `${API_URL}/reference`;
 export const GITHUB_URL = "https://github.com/bcanfield/cronicorn";
