8190f6983f11d6f649445c36449caafddd91a300
CANFIELD.BRANDIN.W.1078680373.E <bcanfield@miletwo.us>
2025-08-15 13:51:32 -0400

    move prod to node from bun

--- docker/start-api.sh
+++ docker/start-api.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-bunx drizzle-kit migrate --config=/app/apps/api/dist/drizzle.config.js
-bun run /app/apps/api/dist/src/index.js
+pnpm run migrate:prod
+node /app/apps/api/dist/src/index.js
